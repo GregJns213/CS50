df5d26104a54375282c5537f121f6ee92c0ff40a
GregJns213 <gregjns213@gmail.com>
2015-10-25 08:32:14 -0400

    mario is shaping up

--- problem_set_1/mario.c
+++ problem_set_1/mario.c
@@ -3,16 +3,17 @@
 
 int main (void)
 {
-    printf("Please give me the height of your blocks: ");
+    printf("height: ");
     int x = GetInt();
     
-    if (x > 23)
+    int y;
+    for (y = 0; y <= x ; y++)
     {
-        printf("That number was over 23! Please give me a non-negative number less than or equal to 23: "); 
-    }
-    
-    else if (x <= 23)
-    {
-        printf("Height: %i\n", x);
-    }
+         int z;
+         for (z = 0; z <= x ; z++)
+        {
+            printf("#");
+        }
+        printf("\n");
+     }
 }
